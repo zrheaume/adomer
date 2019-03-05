56a06f50a3bd7561b797ef04bf818e7f2f907237
zrheaume <zachary.a.rheaume@gmail.com>
2019-03-05 14:51:29 -0500

    Route testing

--- server/routes/apiRoutes.js
+++ server/routes/apiRoutes.js
@@ -1,8 +1,8 @@
 // action routes
 // acions =>
-   // login
-   // logout
-   // signup
+// login
+// logout
+// signup
 //
 
 import express from "express"
@@ -11,18 +11,19 @@ import * as session from "../models/cache/Session"
 const router = express.Router()
 
 router.get("/details/:session", (req, res) => {
-   session.use(req.params.session).then((data) => {
-      let toClient = {
-         username: data.username,
-         apps: data.apps
-      }
-      res.json(toClient)
-   })
+   res.send("ok!")
+   // session.use(req.params.session).then((data) => {
+   //    let toClient = {
+   //       username: data.username,
+   //       apps: data.apps
+   //    }
+   //    res.json(toClient)
+   // })
 })
 
 router.post('/apps', (req, res) => {
    // console.log(req.body)
-   userController.addApp(req.body)
+   // userController.addApp(req.body)
    res.send("ok!")
 })
 
