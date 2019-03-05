e09d4602f2d7000e6943398c9b9abec97cf58e6b
zrheaume <zachary.a.rheaume@gmail.com>
2019-03-05 14:15:13 -0500

    Easyroutes

--- server/routes/actRoutes.js
+++ server/routes/actRoutes.js
@@ -1,28 +1,30 @@
 // action routes
 // acions =>
-   // login
-   // signup
-   // addClient
+// login
+// signup
+// addClient
 
 import express from "express"
 import * as userController from "../controllers/userController"
 const router = express.Router()
 
 router.post("/signup", function (req, res) {
-   userController.addNew(req.body)
-      .then(user => res.json(user))
-      .catch(err => {
-         res.send(err)
-      })
+   res.send("ok")
+   // userController.addNew(req.body)
+   //    .then(user => res.json(user))
+   //    .catch(err => {
+   //       res.send(err)
+   //    })
 })
 
 router.post("/login", function (req, res) {
-   userController.getExisting(req.body)
-      .then((user) => {
-         res.json({ session: user.session })
-      }).catch((err) => {
-         res.send(err)
-      })
+   res.send("ok")
+   // userController.getExisting(req.body)
+   //    .then((user) => {
+   //       res.json({ session: user.session })
+   //    }).catch((err) => {
+   //       res.send(err)
+   //    })
 })
 
 router.put("/addClient", function (req, res) {
