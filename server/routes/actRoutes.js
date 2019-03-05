c517fea277356e83ffd5a6d670ff46114d9656b5
zrheaume <zachary.a.rheaume@gmail.com>
2019-03-05 14:59:24 -0500

    Route testing

--- server/routes/actRoutes.js
+++ server/routes/actRoutes.js
@@ -9,22 +9,22 @@ import * as userController from "../controllers/userController"
 const router = express.Router()
 
 router.post("/signup", function (req, res) {
-   res.send("ok")
-   // userController.addNew(req.body)
-   //    .then(user => res.json(user))
-   //    .catch(err => {
-   //       res.send(err)
-   //    })
+   // res.send("ok")
+   userController.addNew(req.body)
+      .then(user => res.json(user))
+      .catch(err => {
+         res.send(err)
+      })
 })
 
 router.post("/login", function (req, res) {
-   res.send("ok")
-   // userController.getExisting(req.body)
-   //    .then((user) => {
-   //       res.json({ session: user.session })
-   //    }).catch((err) => {
-   //       res.send(err)
-   //    })
+   // res.send("ok")
+   userController.getExisting(req.body)
+      .then((user) => {
+         res.json({ session: user.session })
+      }).catch((err) => {
+         res.send(err)
+      })
 })
 
 router.put("/addClient", function (req, res) {
--- server/routes/apiRoutes.js
+++ server/routes/apiRoutes.js
@@ -11,19 +11,19 @@ import * as session from "../models/cache/Session"
 const router = express.Router()
 
 router.get("/details/:session", (req, res) => {
-   res.send("ok!")
-   // session.use(req.params.session).then((data) => {
-   //    let toClient = {
-   //       username: data.username,
-   //       apps: data.apps
-   //    }
-   //    res.json(toClient)
-   // })
+   // res.send("ok!")
+   session.use(req.params.session).then((data) => {
+      let toClient = {
+         username: data.username,
+         apps: data.apps
+      }
+      res.json(toClient)
+   })
 })
 
 router.post('/apps', (req, res) => {
    // console.log(req.body)
-   // userController.addApp(req.body)
+   userController.addApp(req.body)
    res.send("ok!")
 })
 
